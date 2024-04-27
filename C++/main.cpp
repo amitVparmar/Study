8a46d79f05ef00bbe35eab0f4e6b4c465402dac0
amitVparmar <amitparmar771991@gmail.com>
2024-04-27 23:45:51 +0530

    first commit from ubuntu 24 LTS

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    auto result = (30 <= 20) > 0;
+    auto result = (30 <=> 20) > 0;
     std::cout << result << std::endl;
     std::cout << "Hello World!" << std::endl;
     return 0;
