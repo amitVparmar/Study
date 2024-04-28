929420d6c45ee3c7d6f7a4cceff9eddd45329c8d
amitVparmar <amitparmar771991@gmail.com>
2024-04-28 18:44:36 +0530

    error understanding in cpp codes

--- C++/First_CPP_Code/main.cpp
+++ C++/First_CPP_Code/main.cpp
@@ -6,7 +6,13 @@ consteval int get_value(){
 }
 //Main function is the entry point of the program
 int main(){
-    std::cout << "Hello World!" << std::endl;
-    std::cout << get_value();
+    std::cout << "Hello World!" << std::endl
+    ; //semi-colon can be anywhere between 2 code lines
+    std::cout << get_value(); //without endl
+
+    //runtime error
+    //int i = 7/0;
+    //std::cout << i << std::endl; //Error message : Floating point exception (core dumped)
+
     return 0;
 }
