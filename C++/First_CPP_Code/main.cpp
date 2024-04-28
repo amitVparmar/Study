5fd40fb445ec6e1b6f63adeeec928abd4b2766c4
amitVparmar <amitparmar771991@gmail.com>
2024-04-28 18:26:31 +0530

    comments in cpp codes

--- C++/First_CPP_Code/main.cpp
+++ C++/First_CPP_Code/main.cpp
@@ -1,10 +1,12 @@
+//This brings IOSTREAM library
 #include <iostream>
 
 consteval int get_value(){
     return 3;
 }
-
+//Main function is the entry point of the program
 int main(){
     std::cout << "Hello World!" << std::endl;
+    std::cout << get_value();
     return 0;
 }
